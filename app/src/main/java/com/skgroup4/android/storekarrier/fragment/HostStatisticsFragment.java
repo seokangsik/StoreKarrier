e3d3d22f4d822ad5074037b4e8e2e6afaec3028a
seokangsik <kaing1125@gmail.com>
2017-07-30 17:05:16 +0900

    add host mode

---
+++ app/src/main/java/com/skgroup4/android/storekarrier/fragment/HostStatisticsFragment.java
@@ -0,0 +1,27 @@
+package com.skgroup4.android.storekarrier.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.skgroup4.android.storekarrier.R;
+
+/**
+ * Created by Seo on 2017-07-30.
+ */
+
+public class HostStatisticsFragment extends Fragment {
+    public HostStatisticsFragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_host_statistic,container, false);
+        return view;
+    }
+}
--- app/src/main/java/com/skgroup4/android/storekarrier/fragment/ProfileFragment.java
+++ app/src/main/java/com/skgroup4/android/storekarrier/fragment/ProfileFragment.java
@@ -1,12 +1,15 @@
 package com.skgroup4.android.storekarrier.fragment;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.skgroup4.android.storekarrier.HostModeActivity;
 import com.skgroup4.android.storekarrier.R;
 
 /**
@@ -17,11 +20,21 @@ public class ProfileFragment extends Fragment {
     public ProfileFragment(){
 
     }
-
+    LinearLayout layout;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        layout = (LinearLayout) view.findViewById(R.id.host_mode_layout);
+
+        layout.setOnClickListener(mListener);
         return view;
     }
+    View.OnClickListener mListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getActivity(), HostModeActivity.class);
+            startActivity(intent);
+        }
+    };
 }
